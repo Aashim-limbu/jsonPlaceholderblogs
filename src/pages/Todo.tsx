600f2ff9cd49d0c128d47e4c8ec81bae8ac72c77
Aashim Limbu <thelastmanarmy37@gmail.com>
2023-11-17 23:55:02 +0545

    added the search todo feature

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -8,29 +8,39 @@ import { getTodos } from "../api/getTodos";
 import { Todo } from "../interface/Todo.interface";
 import Inputs from "../components/Inputs";
 import Button from "../components/Button";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 type ResponseType = {
-    todos:Todo[]
-    query?:string
-}
+	todos: Todo[];
+	query?: string | undefined;
+};
 export function Todos() {
-	const {todos,query}: ResponseType = useLoaderData() as ResponseType;
-    useEffect(() => {
-
-    }, [query])
+	const { todos, query }: ResponseType = useLoaderData() as ResponseType;
+	const InputRef = useRef<HTMLInputElement>(document.createElement("input"));
+	useEffect(() => {
+		if (query) InputRef.current.value = query;
+	}, [query]);
 	return (
 		<div>
 			<div className="flex mb-7 justify-between py-2 border-b-2 border-neutral-600">
 				<h1 className="text-5xl font-bold">Todos</h1>
 				<Link to="/new">
-					<Button>New</Button>
+					<Button type="button">New</Button>
 				</Link>
 			</div>
 			<Form className="flex w-full gap-x-2">
 				<div className="flex-1">
-					<Inputs type="search" defaultValue={query} name="query" label="Search" />
+                    {/* basically to tie the things with the actual component itself we use the input ref with the useEffect  */}
+					<Inputs
+						type="search"
+						defaultValue={query}
+						InputRef={InputRef}
+						name="query"
+						label="Search"
+					/>
 				</div>
-				<Button className="ml-auto">Search</Button>
+				<Button type="submit" className="ml-auto">
+					Search
+				</Button>
 			</Form>
 			{todos.map(({ id, title, completed }) => {
 				return (
@@ -44,9 +54,9 @@ export function Todos() {
 }
 function loader({ request: { signal, url } }: LoaderFunctionArgs) {
 	const searchParams = new URL(url).searchParams;
-	const query:string|undefined = searchParams.get("query") || "";
+	const query: string | undefined = searchParams.get("query") || "";
 
-	return getTodos({ signal },query);
+	return getTodos({ signal }, query);
 }
 export const TodoRoute = {
 	element: <Todos />,
